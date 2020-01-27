e41b6ce0f4bc0a5dd6341564d322e18d63088514
Testing_author <testing_beta@mail.com>
2020-01-27 10:42:52 +0200

    Initial Commit

---
+++ Social media project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social media project
+//
+//  Created by DDDD on 27/01/2020.
+//  Copyright © 2020 MeerkatWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
